ba09e1eece4ee85b6a66b45044e3928901d6c857
KateRiinaO <orrava.kate@gmail.com>
2022-04-22 09:09:29 +0300

    Deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather application</h1>
+        <button className="btn btn-primary">hi</button>
       </header>
+      <footer>
+        <p>
+          This project was coded by Katerīna Orrava and is open-sourced on{" "}
+          <a href="/" target="_blank">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="/" target="_blank">
+            Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
-
-export default App;

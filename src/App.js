7439ef344d89193f31c4ecdef7044b31137d8b53
KateRiinaO <orrava.kate@gmail.com>
2022-04-22 09:32:58 +0300

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather application</h1>
-        <button className="btn btn-primary">hello</button>
-      </header>
-      <footer>
-        <p>
-          This project was coded by Katerīna Orrava and is open-sourced on{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/KateRiinaO/app-weather"
-          >
-            GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://dapper-lokum-52cb3b.netlify.app/"
-          >
-            Netlify
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather application</h1>
+          <button className="btn btn-primary">hello</button>
+        </header>
+        <footer>
+          <p>
+            This project was coded by Katerīna Orrava and is open-sourced on{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/KateRiinaO/app-weather"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://dapper-lokum-52cb3b.netlify.app/"
+            >
+              Netlify
+            </a>
+          </p>
+        </footer>
+      </div>{" "}
     </div>
   );
 }
